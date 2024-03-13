c6dc184fcb84419b114cdbafe46163121a7e6bc6
nhantran3395 <thanhnhan33.95@gmail.com>
2024-03-13 21:06:00 +0700

    feat: render code snipper in blog content

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -2,6 +2,7 @@ import React from "react";
 import { MDXRemote } from "next-mdx-remote/rsc";
 
 import BlogHero from "@/components/BlogHero";
+import CodeSnippet from "@/components/CodeSnippet";
 import { loadBlogPost } from "@/helpers/file-helpers";
 
 import styles from "./postSlug.module.css";
@@ -25,7 +26,12 @@ async function BlogPost({ params }) {
         publishedOn={post.frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={post.content} />
+        <MDXRemote
+          source={post.content}
+          components={{
+            pre: (props) => <CodeSnippet {...props} />,
+          }}
+        />
       </div>
     </article>
   );
