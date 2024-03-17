24854d3d9d14b6578e93fa5a8bb7f0945441a21c
nhantran3395 <thanhnhan33.95@gmail.com>
2024-03-17 14:13:45 +0700

    feat: add dark/light mode toggle

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,35 @@
-import React from 'react';
-import clsx from 'clsx';
-import { Rss, Sun, Moon } from 'react-feather';
+"use client";
+import React from "react";
+import Cookie from "js-cookie";
+import clsx from "clsx";
+import { Rss, Sun, Moon } from "react-feather";
 
-import Logo from '@/components/Logo';
-import VisuallyHidden from '@/components/VisuallyHidden';
+import Logo from "@/components/Logo";
+import VisuallyHidden from "@/components/VisuallyHidden";
+import { LIGHT_TOKENS, DARK_TOKENS, COOKIES } from "@/constants";
 
-import styles from './Header.module.css';
+import styles from "./Header.module.css";
 
-function Header({ theme, className, ...delegated }) {
+function Header({ initialTheme, className, ...delegated }) {
+  const [theme, setTheme] = React.useState(initialTheme);
+
+  function toggleTheme() {
+    const next = theme === "light" ? "dark" : "light";
+    const newTokens = next === "light" ? LIGHT_TOKENS : DARK_TOKENS;
+    const root = document.documentElement;
+    root.setAttribute("data-color-theme", next);
+    Object.entries(newTokens).forEach(([key, value]) => {
+      root.style.setProperty(key, value);
+    });
+
+    setTheme(next);
+    Cookie.set(COOKIES.THEME, next, {
+      expires: 1000,
+    });
+  }
+
   return (
-    <header
-      className={clsx(styles.wrapper, className)}
-      {...delegated}
-    >
+    <header className={clsx(styles.wrapper, className)} {...delegated}>
       <Logo />
 
       <div className={styles.actions}>
@@ -21,18 +38,18 @@ function Header({ theme, className, ...delegated }) {
             size="1.5rem"
             style={{
               // Optical alignment
-              transform: 'translate(2px, -2px)',
+              transform: "translate(2px, -2px)",
             }}
           />
-          <VisuallyHidden>
-            View RSS feed
-          </VisuallyHidden>
+          <VisuallyHidden>View RSS feed</VisuallyHidden>
         </button>
         <button className={styles.action}>
-          <Sun size="1.5rem" />
-          <VisuallyHidden>
-            Toggle dark / light mode
-          </VisuallyHidden>
+          {theme === "light" ? (
+            <Sun size="1.5rem" onClick={toggleTheme} />
+          ) : (
+            <Moon size="1.5rem" onClick={toggleTheme} />
+          )}
+          <VisuallyHidden>Toggle dark / light mode</VisuallyHidden>
         </button>
       </div>
     </header>
